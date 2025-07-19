62c2ad0587252c064714644250e8da9853f14f65
mirzashakil313-ship-it <mirzashakil313@gmail.com>
2025-07-19 13:48:23 +0530

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { createPayment } = require('../controllers/paymentController');
+const { createOrder } = require('../controllers/paymentController');
 
-router.post('/pay', createPayment);
+router.post('/create-order', createOrder);
 
 module.exports = router;

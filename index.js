cc355946cda5f9ee47f380d29b154e28f9297fca
mirzashakil313-ship-it <mirzashakil313@gmail.com>
2025-07-19 13:56:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const paymentRoutes = require('./routes/paymentRoutes');
-const payoutRoutes = require('./routes/payoutRoutes');
+const paymentRoutes = require('./routes/payment');
+const payoutRoutes = require('./routes/payout'); // ✅ sahi
+
 
 dotenv.config();
 
